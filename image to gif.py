1dc7cba6c4629386f49474ad52257ee450eceeb0
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-30 20:58:29 +0530

    Add comments for clarity in GIF conversion script
    Added comments to clarify video path and GIF file name.

--- image to gif.py
+++ image to gif.py
@@ -1,5 +1,8 @@
 from moviepy import VideoFileClip
 
+#add your video patha to convert into GIF 
 clip = VideoFileClip(r"C:\Users\Dell\OneDrive\Documents\Image Website\WhatsApp Video 2026-01-26 at 4.58.22 PM.mp4")
+#give the name of the GIF file with Extention ".gif"
 clip.write_gif("gif_2.gif")
 clip.close()
+
